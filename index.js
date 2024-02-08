a34fb26afdd887e5791fb9b662a8a7f6ea8ea8fe
HelenIsted <139465011+HelenIsted@users.noreply.github.com>
2024-02-08 16:51:48 +0000

    Added JS
    Added JS so that correct date and time are shown for different locations.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+setInterval(function () {
+  //London
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
 
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+  londonDateElement.innerHTML = londonTime.format("dddd Do MMMM YYYY");
+  londonTimeElement.innerHTML = londonTime.format("h:mm:s[<small>]a[</small>]");
+  //Sydney
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+  sydneyDateElement.innerHTML = sydneyTime.format("dddd Do MMMM YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:s[<small>]a[</small>]");
+  //Paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = parisTime.format("dddd Do MMMM YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:s[<small>]a[</small>]");
+}, 1000);
